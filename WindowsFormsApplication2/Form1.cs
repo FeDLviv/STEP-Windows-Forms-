6a0009f0f56d1f2c4cd3f054d0ff657512cd0dbc
FeDLviv <fed.lviv@gmail.com>
2016-11-01 14:12:20 +0200

    Add class works

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        Point start;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //PANEL GROUPBOX FLOWLAYOUTPANEL
+            //FLOWLAYOUTPANEL
+            FlowLayoutPanel panel = new FlowLayoutPanel();
+            panel.AutoScroll = true;
+            panel.BackColor = Color.Plum;
+            panel.Size = Size;
+            panel.Parent = this;
+            panel.MouseDown += (obj, ea) => { start = ea.Location; };
+            panel.MouseMove += (obj, ea) => 
+            {
+                if (ea.Button == MouseButtons.Left)
+                {
+                    Point cur = ea.Location;
+                    this.Left += cur.X - start.X;
+                    this.Top += cur.Y - start.Y;
+                }
+            };
+
+            //LISTBOX
+            ListBox list = new ListBox();
+            list.SelectionMode = SelectionMode.One;
+            list.MultiColumn = false;
+            list.Items.Add("Кузя");
+            list.Items.Add(new Point2D(3, 3));
+            list.Items.AddRange(new string[] { "Федько", "Олексій" });
+            list.Parent = panel;
+
+            //CHECKEDLISTBOX
+            CheckedListBox chBox = new CheckedListBox();
+            chBox.Items.AddRange(new string[] { "Лось", "Ведмідь", "Білка", "Пес" });
+            chBox.Parent = panel;
+            chBox.KeyDown += (obj, ea) =>
+            {
+                if (ea.Control && ea.KeyCode == Keys.C)
+                {
+                    CheckedListBox temp = obj as CheckedListBox;
+                    if (temp.CheckedItems.Count > 0)
+                    {
+                        foreach (object x in temp.CheckedItems)
+                        {
+                            list.Items.Add(x);
+                        }
+                    }
+                }
+            };
+
+            //!!!OnParent
+            //COMBOBOX
+            ComboBox cBox = new ComboBox();
+            cBox.Items.AddRange(new string[] { "кава", "чай", "сік" });
+            cBox.Parent = panel;
+            
+            //LABEL
+            Label label = new Label();
+            label.Text = "СУПЕР лейба";
+            label.BorderStyle = BorderStyle.FixedSingle;
+            label.Parent = panel;
+
+            //BUTTON
+            Button button = new Button();
+            button.Text = "Click";
+            button.Parent = panel;
+            button.Click += (obj, ea) => 
+            {
+                if (list.SelectedIndex > 0)
+                {
+                    MessageBox.Show(list.SelectedItem.ToString());
+                }
+            };
+
+            //!!!OnParent
+            //TOOLTIP
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            toolTip1.ShowAlways = true;
+            toolTip1.SetToolTip(button, "ФЕДЬКО");
+            toolTip1.SetToolTip(label, "КУЗЯ");
+
+            //HANDLER
+            MouseEventHandler handler = new MouseEventHandler(OnParent);
+            for (int i = 0; i < panel.Controls.Count; i++)
+            {
+                //panel.Controls[i].MouseMove += handler;
+            }
+
+            this.SizeChanged += (obj, ea) => { panel.Size = Size; };
+        }
+
+        private void OnParent(object obj, MouseEventArgs mea)
+        {
+            (obj as Control).Capture = false;
+            Message msg = Message.Create(Handle, WM_NCLBUTTONDOWN, (IntPtr) HT_CAPTION, IntPtr.Zero);
+            WndProc(ref msg);
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    public class Point2D
+    {
+        int x;
+        int y;
+
+        public Point2D(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        List<Student> list = new List<Student>()
+            {
+                new Student(1, "Кузьмінський", 1990, "s25-vp1"),
+                new Student(2, "Власов", 1989, null),
+                new Student(3, "Пристайко", 2000, "s25-vp1"),
+                new Student(4, "Пишний", 1985, "s25-vp1")
+            };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //FORM
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            //FLOWLAYOUTPANEL
+            FlowLayoutPanel panel = new FlowLayoutPanel();
+            panel.BackColor = Color.Beige;
+            panel.AutoSize = true;
+            panel.Dock = DockStyle.Fill;
+            panel.Parent = this;
+
+            //PROPERTYGRID
+            PropertyGrid pg = new PropertyGrid();
+            pg.SelectedObject = list[0];
+            pg.Margin = new Padding(50, 10, 50, 20);
+            pg.MinimumSize = new Size(300, 300);
+            pg.Parent = panel;
+
+            //PROGRESSBAR
+            ProgressBar pr = new ProgressBar();
+            pr.Minimum = 0;
+            pr.Maximum = list.Count -1;
+            pr.Step = 1;
+            pr.AutoSize = true;
+            pr.Parent = panel;
+
+            //NUMERICUPDOWN
+            NumericUpDown upD = new NumericUpDown();
+            upD.Minimum = 0;
+            upD.Maximum = list.Count - 1;
+            upD.Increment = 1;
+            upD.DecimalPlaces = 0;
+            upD.ReadOnly = true;
+            upD.AutoSize = true;
+            upD.Parent = panel;
+            upD.ValueChanged += (obj, ea) =>
+            {
+                pg.SelectedObject = list[(int) upD.Value];
+            };
+
+            //DOMAINUPDOWN
+            DomainUpDown upDom = new DomainUpDown();
+            object[] ar = typeof(Color).GetProperties(BindingFlags.Public | BindingFlags.Static);
+            upDom.Items.Add(ar[5]);
+            upDom.ReadOnly = true;
+            upDom.AutoSize = true;
+            upDom.Parent = panel;
+            upDom.SelectedItemChanged += (obj, ea) =>
+            {
+                string temp = upDom.SelectedItem.ToString();
+                string color = temp.Remove(0, 21);
+                //BackColor = (Color) ((typeof(Color)).GetProperty(color)).GetValue(null, null);
+            };
+            
+
+            //BUTTON
+            Button but = new Button() { Text = "НАСТУПНИЙ", BackColor = Color.Gray, AutoSize = true, Parent = panel };
+            but.MouseDown += (obj, ea) =>
+            {
+                if (ea.Button == MouseButtons.Left) 
+                {
+                     pr.PerformStep();
+                }
+                else 
+                {
+                    if (pr.Value >= pr.Step)
+                    {
+                        pr.Value -= pr.Step;
+                    }
+                }
+            };
+        }
+
+
+    }
+
+    class Student
+    {
+        //не відображати властивість
+        [Browsable(false)] 
+        public int ID { set; get; }
+        //ім'я властивості при відображенні, опис властивості, тільки читання властивості
+        [DisplayName("Ім'я"), Description("Прізвище студента"), ReadOnly(true)]
+        //[TypeConverter(typeof(JobTitleTypeConverter))]
+        public string Name { set; get; }
+        //ім'я властивості при відображенні, опис властивості, категорія до якої належить властивість
+        [DisplayName("Рік"), Description("Рік народження студента"), Category("Решта даних")]
+        public int Year { get; set; }
+        //ім'я властивості при відображенні, опис властивості, категорія до якої належить властивість
+        [DisplayName("Група"), Description("Група в якій навчається студент"), Category("Решта даних")]
+        public string Group { get; set; }
+
+        public Student(int id, string name, int year, string group)
+        {
+            ID = id;
+            Name = name;
+            Year = year;
+            Group = group;
+        }
+    }
+}
