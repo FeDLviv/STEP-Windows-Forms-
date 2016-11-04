8592b63d3a56157702ff5fe65cb8f8cd89059ef7
FeDLviv <fed.lviv@gmail.com>
2016-11-04 10:36:33 +0200

    Add class work 2016-11-03

---
+++ Classwork2016-11-03/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    //MENU
+    public partial class Form1 : Form
+    {
+        Color color;
+        public Form1()
+        {
+            InitializeComponent();
+            color = BackColor;
+                        
+            //MENUSTRIP
+            MenuStrip mainMenu = new MenuStrip();
+
+            //TOOLSTRIPMENUITEM, TOOLSTRIPSEPARATOR, TOOLSTRIPTEXTBOX, TOOLSTRIPCOMBOBOX
+            ToolStripMenuItem menuF = new ToolStripMenuItem("File");
+            ToolStripMenuItem menuS = new ToolStripMenuItem("Save");
+            ToolStripSeparator menuSep = new ToolStripSeparator();
+            ToolStripMenuItem menuC = new ToolStripMenuItem("Yellow color");
+            ToolStripTextBox menuT = new ToolStripTextBox();
+            ToolStripComboBox menuCB = new ToolStripComboBox();
+            ToolStripMenuItem menuE = new ToolStripMenuItem("Exit");
+
+            menuF.DropDownItems.Add(menuS);
+            menuF.DropDownItems.Add(menuSep);
+            menuF.DropDownItems.Add(menuC);
+            menuF.DropDownItems.Add(menuT);
+            menuF.DropDownItems.Add(menuCB);
+
+            menuS.ShortcutKeys = Keys.Alt | Keys.S;
+            menuS.ShowShortcutKeys = true;
+            menuS.Click += (obj, ea) => { MessageBox.Show("Save file... ;)"); };
+
+            menuC.CheckOnClick = true;
+            menuC.CheckedChanged += (obj, ea) =>
+            {
+                if (menuC.Checked)
+                {
+                    BackColor = Color.Yellow;
+                }
+                else
+                {
+                    BackColor = color;
+                }
+            };
+
+            menuE.Click += (obj, ea) => { Close(); };
+            
+            mainMenu.Items.Add(menuF);
+            mainMenu.Items.Add(menuE);
+
+            mainMenu.Parent = this;
+        }
+    }
+}
